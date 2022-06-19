77243c951644a8ff71605b74e850b32d255d9a16
Muflihat Abdulmajid <muflihaabd@gmail.com>
2022-06-19 11:59:04 +0100

    Update

--- src/Plate.jsx
+++ src/Plate.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-function Plate(){
-    return(
-        <div>
-            <div className="LicensePlate text-center"><h1><strong>GJ 03LK 0563</strong></h1></div>
-        </div>
-    )
-};
-export default Plate;
+export default function Plate() {
+  const val1 = (Math.floor(Math.random() * 100) + 100).toString().substring(1);
+  const val2 = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+  
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (var i = 0; i < 2; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return (
+    <div className="LicensePlate text-center">
+      <h1> {val1} - {text} - {val2}  </h1>
+    </div>
+  );
+}
